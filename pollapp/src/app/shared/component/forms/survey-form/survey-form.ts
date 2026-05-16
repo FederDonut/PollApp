273d0482f57c848a58d9ceb277aadad40a818681
raphael <raphael.zwick8@gmail.com>
2026-05-16 12:29:17 +0200

    add ne delte function for a singel question answer

--- pollapp/src/app/shared/component/forms/survey-form/survey-form.ts
+++ pollapp/src/app/shared/component/forms/survey-form/survey-form.ts
@@ -39,10 +39,14 @@ export class SurveyForm {
     this.questions.update((currentQuestion) => [...currentQuestion, newQuestion]);
 
     console.log('Anzahl der Fragen: ', this.questions());
-    //this.questionCataloge.push(this.questions());
-    //console.log('FrageKatalog:', this.questionCataloge);
+    //this.generateStartAnswer();
   }
 
+  generateStartAnswer() {
+    console.log(this.questions().length);
+    const array = this.questions();
+    //const gesuchterUser = array.find((user) => console.log(user.answers));
+  }
   // id müssen wieder angepasst werden --> ngOnInit()
   deleteWholeQuestion(id: number) {
     //console.log('Frage:', id, ' soll gelöscht werden ');
@@ -75,6 +79,7 @@ export class SurveyForm {
   //}
 
   createNewAnswerObject(currentAnswers: number) {
+    console.log(currentAnswers);
     const id = 'id-' + currentAnswers;
     const newAnser: AnswerInterface = {
       id: id,
@@ -103,8 +108,31 @@ export class SurveyForm {
     });
   }
 
-  deleteAnswerInSingleQuestion(event: {}) {
-    console.log(event);
+  //deleteAnswerInSingleQuestion(event: { questionID: number; answerID: number }) {
+  //  console.log(event);
+  //  console.log('Frage ID:', event.questionID, 'Antwort ID: ', event.answerID);
+  //  console.log(this.questions());
+  //
+  //}
+
+  //Spread Operator = nimm das objket q brich es auf und kopiere alle Eigenschaften(id,usw)
+  //in das folgende Opbjekt das Komma und das danach aufgeführet answer bedeutet kopiere alles auser diesen wert
+  //diesen überschreibst du mit was ich dir mitgebe
+  deleteAnswerInSingleQuestion(event: { questionID: number; answerID: number }) {
+    // übergebe dem Signal das gesamte Array Objekt
+    this.questions.update((currentQuestions) =>
+      currentQuestions.map((q) => {
+        // 1. Finde die richtige Frage
+        if (q.id === event.questionID) {
+          return {
+            ...q,
+            // 2. Filter die Antworten dieser Frage anhand des Indexes
+            answers: q.answers.filter((_, index) => index !== event.answerID),
+          };
+        }
+        return q;
+      }),
+    );
   }
 
   //outsourcing in service
